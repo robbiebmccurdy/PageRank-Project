1c83bce028caac6d74c7e7a7b1df5d5acb0841a2
Robbie McCurdy <robbiebmccurdy@gmail.com>
2023-10-03 08:04:20 -0500

    Finished Section 1A and Added Test CSVs
    Added test csvs to match the csvs stated in the project pdf by the professor. Made pandas dataframe that skipped the first row in the edges csv so we don't contain strings in our array. Converted to a numpy array and then inserted it into link matrix, divided by column sums, and printed.

--- main.py
+++ main.py
@@ -1,37 +1,64 @@
 import sys
-
-# Checking to see if the program was called correctly with the correct arguments
-=======
+import numpy as np
 import pandas as pd
->>>>>>> main
->>>>>>> Stashed changes
+
+# Checking to see if the command line arguements to call the program are correct, if not we will break the program and give an error message
 
 if len(sys.argv) != 3:
-	print("Incorrect amount of arguments. Please only input the program name, the node.csv, and edge.csv file in the command line argument when calling this program.")
+	print("ERROR: Incorrect amount of arguments. Please only input the program name, the node.csv, and edge.csv file in the command line argument when calling this program.")
 	sys.exit()
 elif ".csv" not in sys.argv[1] or ".csv" not in sys.argv[2]:
-	print("Please enter the correct file format (.csv) ")
+	print("ERROR: Please enter the correct file format (.csv) ")
 	sys.exit()
 elif "node" not in sys.argv[1]:
-	print("Incorrect file! Please put the node.csv file first.")
+	print("ERROR: Incorrect file! Please put the node.csv file first.")
 	sys.exit()
 elif "edge" not in sys.argv[2]:
-	print("Incorrect file! Please put the edge.csv file second.")
+	print("ERROR: Incorrect file! Please put the edge.csv file second.")
 	sys.exit()
 else:
 	print("File checks passed. Starting program...")
 
-print("\nFirst File: ")
-print(sys.argv[1])
-print("\nSecond File: ")
-print(sys.argv[2])
+# PART 1
+
+# SECTION A
+
+# Grabs the file names from the arguements and reads the csv files, stores them in their own variables
+
+nodeFile = pd.read_csv(sys.argv[1], header=None).values.flatten()
+nodeFile = np.delete(nodeFile, 0)
+edgeFile = pd.read_csv(sys.argv[2], skiprows=1, header=None) # Skips the first row to avoid having Strings in the array
+edgeArray = edgeFile.values
+print(edgeArray)
+
+# Grabs the length of our nodes file (n) and then creates a blank matrix that is size (n x n)
 
 nodes = len(nodeFile)
+linkMatrix = np.zeros((nodes, nodes)) 
 
-print("Node file: \n")
-print(nodeFile.read())
-print("\nEdge file: \n")
-print(edgeFile.read())
+# Iterates through the array grabbed from the edges csv file then inserts them into a link matrix
+
+for i, j in edgeArray:
+	if j == 0:
+		linkMatrix[j,i] += 1
+	else:
+		linkMatrix[j-1,i-1] += 1
+
+# Sums the columns and divides the values in the columns by the sum of the columns 
+# (basically if we have 0,1 and 0,3 in the edges .csv we fill both of those spots with a 1, then later we divide them by 2 giving us 0.50)
+
+col_sums = linkMatrix.sum(axis=0, keepdims=True)
+col_sums[col_sums == 0] = 1
+linkMatrix = linkMatrix / col_sums
+
+# Printing Link Matrix with the formatting requested
+
+print("\nLink Matrix: \n")
+for row in linkMatrix:
+    formatted_row = ", ".join(["{:.2f}".format(val) for val in row])
+    print(formatted_row)
+
+
 
 print("\nSuccessfully read files.")
 print("\nQuitting program...")
